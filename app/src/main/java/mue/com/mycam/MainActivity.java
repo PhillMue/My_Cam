f044b3284b9217a777457bb6bd0066a5abeeb0f6
PhillMue <muenzuve@gmail.com>
2016-03-21 10:54:13 +0300

    building splash screen

--- app/src/main/java/mue/com/mycam/MainActivity.java
+++ app/src/main/java/mue/com/mycam/MainActivity.java
@@ -1,7 +1,11 @@
 package mue.com.mycam;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,6 +16,8 @@ public class MainActivity extends Activity {
 
     Button button;
     ImageView imageView;
+    static final int CAM_REQUEST = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +28,16 @@ public class MainActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File file = getFile();
+                camera_intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+                startActivityForResult(camera_intent,CAM_REQUEST);
 
             }
         });
     }
 
-    private File_getFile(){
+    private File getFile(){
 
         File folder = new File("sdcard/camera_app");
 
@@ -40,4 +49,11 @@ public class MainActivity extends Activity {
         File image_file = new File(folder,"cam_image.jpg");
         return image_file;
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        String path = "sdcard/camera_app/cam_image.jpg";
+        imageView.setImageDrawable(Drawable.createFromPath(path));
+
+    }
+
 }
