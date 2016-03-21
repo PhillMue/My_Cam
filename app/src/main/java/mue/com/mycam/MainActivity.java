931b664fe090508a637b0e259d5923115f1baec2
PhillMue <muenzuve@gmail.com>
2016-03-21 09:55:22 +0300

    added an if statement

--- app/src/main/java/mue/com/mycam/MainActivity.java
+++ app/src/main/java/mue/com/mycam/MainActivity.java
@@ -2,8 +2,11 @@ package mue.com.mycam;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
@@ -14,6 +17,27 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+        button = (Button) findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.image_view);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+    }
+
+    private File_getFile(){
+
+        File folder = new File("sdcard/camera_app");
+
+        if(!folder.exists())
+        {
+            folder.mkdir();
+        }
+
+        File image_file = new File(folder,"cam_image.jpg");
+        return image_file;
     }
 }
